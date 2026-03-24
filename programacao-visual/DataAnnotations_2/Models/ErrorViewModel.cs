8cc61edd9ca9e53040921bd41b923a3177101090
wallyson machado <machadodelimawallyson@gmail.com>
2026-03-23 23:56:16 -0400

    Refactor code structure for improved readability and maintainability

---
+++ programacao-visual/DataAnnotations_2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DataAnnotations.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
